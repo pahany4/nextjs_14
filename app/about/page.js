6c268e9d2945a42eee4faab3862e1ffb3eab8ca1
paha <pahany4@mail.ru>
2023-12-19 23:16:49 +0700

    meta

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,8 @@
 
+// статическая meta
+export const metadata = {
+  title: "О нас"
+}
 export default function About() {
   return (
     <div>
--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,3 +1,11 @@
+
+// динамическая meta
+export const generateMetadata = async ({params: {id}}) => {
+  return {
+    title: `Пост - ${id}`
+  }
+}
+
 export default function Post({params: {id}}) {
   return (
     <h1>Пост - {id}</h1>
